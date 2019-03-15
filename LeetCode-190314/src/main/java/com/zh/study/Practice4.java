7455de483dba705750e025355d304666ddaa7224
halozixin <975577725@qq.com>
2019-03-15 13:42:33 +0800

    leetCode980

---
+++ LeetCode-190314/src/main/java/com/zh/study/Practice4.java
@@ -0,0 +1,18 @@
+package com.zh.study;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * @auther zh
+ * @data 2019/3/14 20:21
+ */
+public class Practice4 {
+    String str = "good";
+    public static void main(String[] args) {
+       Practice4 practice4 = new Practice4();
+    }
+   public static void pomg(String str){
+        str = "Test";
+    }
+}
